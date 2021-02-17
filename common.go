cc789313263745398c1d2f40dcc683b62280acb1
Rahul Baruri <rahulbaruri1@gmail.com>
2021-02-17 01:59:53 +0100

    added map function

--- common.go
+++ common.go
@@ -14,3 +14,39 @@ func randomer() *rand.Rand {
 	seed := rand.NewSource(time.Now().UnixNano())
 	return rand.New(seed)
 }
+
+//StringToInterfaceSlice converts a slice of string to slice of interface
+func StringToInterfaceSlice(stringSlice []string) []interface{} {
+	interfaceSlice := make([]interface{}, len(stringSlice), len(stringSlice))
+	for i := range stringSlice {
+		interfaceSlice[i] = stringSlice[i]
+	}
+	return interfaceSlice
+}
+
+//StringSlice converts a slice of interface to slice of string
+func StringSlice(interfaceSlice []interface{}) []string {
+	stringSlice := make([]string, len(interfaceSlice), len(interfaceSlice))
+	for i := range interfaceSlice {
+		stringSlice[i] = interfaceSlice[i].(string)
+	}
+	return stringSlice
+}
+
+//IntSlice converts a slice of interface to slice of int
+func IntSlice(interfaceSlice []interface{}) []int {
+	intSlice := make([]int, len(interfaceSlice), len(interfaceSlice))
+	for i := range interfaceSlice {
+		intSlice[i] = interfaceSlice[i].(int)
+	}
+	return intSlice
+}
+
+//Float64Slice converts a slice of interface to slice of int
+func Float64Slice(interfaceSlice []interface{}) []float64 {
+	float64Slice := make([]float64, len(interfaceSlice), len(interfaceSlice))
+	for i := range interfaceSlice {
+		float64Slice[i] = interfaceSlice[i].(float64)
+	}
+	return float64Slice
+}
--- maps.go
+++ maps.go
@@ -1,21 +1,72 @@
 package gofp
 
 /**MAP UTILS
-- keys()
-- values()
-- entries()
-- has()
 - get()
-- pick()
-- omit()
 - merge()
-- empty()
-- mapValues()
-- orderedKeys()
-- orderedValues()
 */
 
 // Keys returns all the keys of any map
-func Keys(mapItem map[string]interface{}) []string {
-	return []string{}
+func Keys(mapData map[string]interface{}) []string {
+	keys := []string{}
+	for key := range mapData {
+		keys = append(keys, key)
+	}
+	return keys
+}
+
+// Values returns all the keys of any map
+func Values(mapData map[string]interface{}) []interface{} {
+	values := []interface{}{}
+	for _, value := range mapData {
+		values = append(values, value)
+	}
+	return values
+}
+
+// Omit returns a new map containing keys that doesn't exists in the provided omittable Keys
+func Omit(mapData map[string]interface{}, omittableKeys []string) map[string]interface{} {
+	newMap := map[string]interface{}{}
+	for key, value := range mapData {
+		if !Contains(StringToInterfaceSlice(omittableKeys), key) {
+			newMap[key] = value
+		}
+	}
+	return newMap
+}
+
+// MapValues returns a map transforming the values applying provided function
+func MapValues(mapData map[string]interface{}, fn func(interface{}) interface{}) map[string]interface{} {
+	newMap := map[string]interface{}{}
+	for key, value := range mapData {
+		newMap[key] = fn(value)
+	}
+	return newMap
+}
+
+// MapKeys returns a map transforming the keys applying provided function
+func MapKeys(mapData map[string]interface{}, fn func(interface{}) interface{}) map[string]interface{} {
+	newMap := map[string]interface{}{}
+	for key, value := range mapData {
+		newKey := fn(key).(string)
+		newMap[newKey] = value
+	}
+	return newMap
+}
+
+// Pick returns a new map with matched keys
+func Pick(mapData map[string]interface{}, keys []string) map[string]interface{} {
+	newMap := map[string]interface{}{}
+	for _, key := range keys {
+		value, ok := mapData[key]
+		if ok {
+			newMap[key] = value
+		}
+	}
+	return newMap
+}
+
+// Has returns all the keys of any map
+func Has(mapData map[string]interface{}, key string) (exists bool) {
+	_, exists = mapData[key]
+	return
 }
